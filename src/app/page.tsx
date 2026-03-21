bf9e42579fb83564b23ca688302fcaa084e2400d
Agustina Guerendiain <agusguere@gmail.com>
2026-03-21 15:19:40 +0100

    reviews comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <Hero />
       <Services />
       <Gallery />
-      <Reviews />
+      {/* <Reviews /> */}
       <Contact />
       <Footer />
       <FloatingContact />
